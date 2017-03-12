4ab9d0bb80f8478c73d61c9f187de44a578a1678
Karim Kidiss <kkidiss@yahoo.fr>
2017-03-12 21:32:26 +0100

    documented the index file

--- weather/index.js
+++ weather/index.js
@@ -3,17 +3,24 @@
 const YQL = require('yql');
 const _ = require('lodash');
 
+/** 
+	@opts is the input of the user 
+	@query the query sent to webservice
+**/
 module.exports = (opts, callback) => {
 	opts = opts || [];
-
 	let query;
 
+/** This defines the default query **/
 	if (_.isEmpty(opts)) {
 		query = new YQL('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="Dhaka, Bangladesh")');
-	} else {
+	} 
+/** Here we parse the input of the user to make the correct request **/
+	else {
 		query = new YQL('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + opts[0] + ', ' + opts[1] + '")');
 	}
 
+/** Execution of the query **/
 	query.exec((err, response) => {
 		if (err) {
 			return callback(err);
